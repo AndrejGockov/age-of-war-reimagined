83725616ee1f82deed2d83b281c53888519c4a09
Martin <martinjovanov2004@gmail.com>
2026-05-20 19:50:30 +0200

    Optimized camera logic

--- Scenes/Game/camera_2d.gd
+++ Scenes/Game/camera_2d.gd
@@ -4,18 +4,22 @@ extends Camera2D
 @export var edge_margin: float = 150.0
 @export var left_limit: float = -2000.0
 @export var right_limit: float = 2000.0
-# camera lol
-func _process(delta: float) -> void:
-	var mouse_x = get_viewport().get_mouse_position().x
-	var screen_width = get_viewport().get_visible_rect().size.x
-	
-	if mouse_x < edge_margin:
-		var str = 1.0 - (mouse_x / edge_margin)
-		global_position.x -= pan_speed * str * delta
-	elif mouse_x > screen_width - edge_margin:
-		var str = (mouse_x - (screen_width - edge_margin)) / edge_margin
-		global_position.x += pan_speed * str * delta
-	global_position.x = clamp(global_position.x, left_limit, right_limit)
+var str: float
+
+var mouse_x
+var screen_width
 
 func _ready() -> void:
-	pass 
+	screen_width = get_viewport().get_visible_rect().size.x
+	
+
+func _process(delta: float) -> void:
+	mouse_x = get_viewport().get_mouse_position().x
+	
+	if mouse_x < edge_margin:
+		str = 1.0 - (mouse_x / edge_margin)
+		global_position.x -= pan_speed * str * delta
+	elif mouse_x > screen_width - edge_margin:
+		str = (mouse_x - (screen_width - edge_margin)) / edge_margin
+		global_position.x += pan_speed * str * delta
+	global_position.x = clamp(global_position.x, left_limit, right_limit)
