bf5eecd1e543bbf5336903fe3f21b47d1c0ffd74
hdon4 <51890226+hdon4@users.noreply.github.com>
2026-08-15 15:21:33 +0200

    Added basic parralax, health bar modifications

--- Entities/Scripts/health_bar.gd
+++ Entities/Scripts/health_bar.gd
@@ -12,10 +12,23 @@ func setup(max_hp: int, curr_hp: int) -> void:
 	bar.max_value = max_hp
 	set_hp(curr_hp)
 
-# set hitpoints
+# set hitpoints and change color
 func set_hp(curr_hp: int) -> void:
 	if not is_inside_tree(): 
 		await ready
-	
+
 	bar.value = curr_hp
-	label.text = str(curr_hp)+"/"+str(int(bar.max_value))
+	label.text = str(curr_hp) + "/" + str(int(bar.max_value))
+
+	var hp_percent := float(curr_hp) / bar.max_value
+
+	var stylebox := bar.get_theme_stylebox("fill").duplicate()
+
+	if hp_percent <= 0.2:
+		stylebox.bg_color = Color.RED
+	elif hp_percent <= 0.5:
+		stylebox.bg_color = Color.YELLOW
+	else:
+		stylebox.bg_color = Color.GREEN
+
+	bar.add_theme_stylebox_override("fill", stylebox)
